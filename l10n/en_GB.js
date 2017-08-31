00f169e35032db1abca55f1360a431e4608e16bc
Hemant Mann <hemant.mann121@gmail.com>
2017-08-31 15:25:11 +0530

    Text Change

--- l10n/en_GB.js
+++ l10n/en_GB.js
@@ -1,7 +1,7 @@
 OC.L10N.register(
     "files_external_ftp",
     {
-        "Dropbox (Fly)" : "Dropbox (Fly)",
+        "Dropbox" : "Dropbox",
         "Client Id" : "Client Id",
         "Client Secret" : "Client Secret",
         "Token" : "Token"
